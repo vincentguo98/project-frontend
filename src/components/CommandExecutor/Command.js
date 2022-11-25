c22d44b73502fb95e32ec1dd00133b7cd2bda267
Wenzheng Guo <gwz1805504@gmail.com>
2022-11-24 19:11:24 -0800

    complete file explorer

--- src/components/CommandExecutor/Command.js
+++ src/components/CommandExecutor/Command.js
@@ -1,28 +1,52 @@
 import {Container, Form, Button, Row, Col} from "react-bootstrap";
+import {
+    mkdir, ls, cat, rm, put, getPartitionLocations, readPartition,
+    PATH, UPLOAD_FILENAME, PARTITION, FIREBASE, MYSQL, MONGDB, FILENAME_LIST
+} from "../../constants/CommandConstants";
 import {useEffect, useState} from "react";
 import  "../../css/style.css"
+import {BuildUrl} from "../../Util/UrlUtil";
 
 export function Command(){
-    const [isFileArguments, setIsFileArguments] = useState(false) ;
-    const [hasSecondArguments, setHasSecondArguments] = useState(false);
 
-    const [command, setCommand] = useState("mkdir");
+    const commandArguments = {
+        [mkdir] : {[PATH]: true},
+        [ls] : {[PATH]: true},
+        [cat]: {[PATH]: true},
+        [rm]: {[PATH]: true},
+        [put]: {[UPLOAD_FILENAME]: true, [PATH]: true, [PARTITION]: true},
+        [getPartitionLocations]: {[PATH]: true},
+        [readPartition]: {[PATH]: true, [PARTITION]: true},
+    }
 
-    const [firstArgument, setFirstArgument] = useState("");
+    const [edfs, setEdfs] = useState(FIREBASE)
 
-    const [secondArgument, setSecondArgument] = useState("");
+    const [command, setCommand] = useState(mkdir);
 
-    const [fileArgument, setFileArgument] = useState("");
+    const [path, setPath] = useState("");
 
+    const [uploadFilename, setUploadFilename] = useState(FILENAME_LIST[0]);
 
-    const chooseCommand = (c) => {
-        setIsFileArguments(false);
-        setHasSecondArguments(false);
-        if (c === "ls") {
-            setIsFileArguments(true);
-            setHasSecondArguments(true);
-        }
-        setCommand(c)
+    const [partition, setPartition] = useState("");
+
+    let params = {
+        "database": edfs,
+        "path": path,
+        "filename": uploadFilename,
+        "partition": partition,
+    }
+
+    const executeCommand = () => {
+        var validQuery = Object.fromEntries(Object.entries(params).filter(([key]) => commandArguments[command][key]))
+
+        console.log(validQuery)
+
+        const fetchURL = BuildUrl(command, edfs, validQuery)
+        console.log(fetchURL)
+
+        fetch(fetchURL)
+            .then((response) => response.json())
+            .then(it => console.log(it))
     }
 
     return (
@@ -30,58 +54,78 @@ export function Command(){
             <Row>
                 <Col xs={2}>
                     <Form.Group>
+                        <Form.Label>EDFS Type</Form.Label>
+                        <Form.Select value={edfs}
+                                     onChange={(e) => {setEdfs(e.target.value)}
+                                     }>
+                            <option value={FIREBASE}>{FIREBASE}</option>
+                            <option value={MYSQL}>{MYSQL}</option>
+                            <option value={MONGDB}>{MONGDB}</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Col>
+
+                <Col xs={2}>
+                    <Form.Group>
                         <Form.Label>Command</Form.Label>
                         <Form.Select value={command}
-                                     onChange={(e) => {chooseCommand(e.target.value)}
+                                     onChange={(e) => {setCommand(e.target.value)}
                         }>
-                            <option value="mkdir">mkdir</option>
-                            <option value="ls">ls</option>
-                            <option value="cat">cat</option>
-                            <option value="rm">rm</option>
-                            <option value="put" >put</option>
-                            <option value="getPartitionLocations">getPartitionLocations</option>
-                            <option value="readPartition">readParition</option>
+                            <option value={mkdir} >mkdir</option>
+                            <option value={ls}>ls</option>
+                            <option value={cat}>cat</option>
+                            <option value={rm}>rm</option>
+                            <option value={put} >put</option>
+                            <option value={getPartitionLocations}>getPartitionLocations</option>
+                            <option value={readPartition}>readPartition</option>
                         </Form.Select>
                     </Form.Group>
                 </Col>
 
                 {
-                    isFileArguments &&
+                    commandArguments[command][PATH] &&
                     <Col>
                         <Form.Group>
-                            <Form.Label>Arguments</Form.Label>
-                            <Form.Control type="file"
-                                          value={fileArgument}
-                                          onChange={(e) =>
-                                          {setFileArgument(e.target.value);
-                                              console.log(e.target.value);
-                                          }
-                            }/>
+                            <Form.Label>{PATH}</Form.Label>
+                            <Form.Control value={path}
+                                          onChange={(e) => setPath(e.target.value)}
+                            />
                         </Form.Group>
                     </Col>
                 }
 
-                <Col>
-                    <Form.Group>
-                        <Form.Label>Arguments</Form.Label>
-                        <Form.Control value={firstArgument}
-                                      onChange={(e) => setFirstArgument(e.target.value)}
-                                     />
-                    </Form.Group>
-                </Col>
+
+
                 {
-                    hasSecondArguments &&
+                    commandArguments[command][UPLOAD_FILENAME] &&
                     <Col>
                         <Form.Group>
-                            <Form.Label>Arguments</Form.Label>
-                            <Form.Control value={secondArgument}
-                                          onChange={(e) => setSecondArgument(e.target.value)}/>
+                            <Form.Label>{UPLOAD_FILENAME}</Form.Label>
+                            <Form.Select value={uploadFilename}
+                                         onChange={(e) => {setUploadFilename(e.target.value)}
+                                         }>
+                                {FILENAME_LIST.map(filename => (
+                                    <option value={filename}>{filename}</option>
+                                ))}
+                            </Form.Select>
                         </Form.Group>
                     </Col>
                 }
+
+                {
+                    commandArguments[command][PARTITION] &&
+                    <Col>
+                        <Form.Group>
+                            <Form.Label>{PARTITION}</Form.Label>
+                            <Form.Control value={partition}
+                                          onChange={(e) => setPartition(e.target.value)}/>
+                        </Form.Group>
+                    </Col>
+                }
+
             </Row>
             <Row style={{marginTop: 20, padding: 10}}>
-                <Button variant={"primary"} type={"submit"} size={"lg"}>Execute</Button>
+                <Button variant={"primary"} type={"submit"} size={"lg"} onClick={() => {executeCommand()}}>Execute</Button>
             </Row>
         </Container>
     )
---
+++ src/constants/CommandConstants.js
@@ -0,0 +1,20 @@
+export const mkdir = "mkdir"
+export const ls = "ls"
+export const cat = "cat"
+export const rm = "rm"
+export const put = "put"
+export const getPartitionLocations = "getPartitionLocations"
+export const readPartition = "readPartition"
+
+export const PATH = "path";
+export const UPLOAD_FILENAME = "filename"
+export const PARTITION = "partition"
+
+export const FIREBASE = "firebase"
+export const MYSQL = "mysql"
+export const MONGDB = "mongodb"
+
+export const FILENAME_LIST = [
+    "california_vaccination.csv",
+    "LA_County_COVID_Testing.csv"
+]
